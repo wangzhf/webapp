a34ae9528b21d8f5521faa4bf1517e90213ec0e0
wangzhf <zhifengtech@gmail.com>
2019-11-17 23:26:19 +0800

    add db crate

--- common/db/src/lib.rs
+++ common/db/src/lib.rs
@@ -1,7 +1,45 @@
+
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use std::env;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+pub mod schema;
+pub mod models;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv::dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+    use crate::schema::posts::dsl::*;
+    use crate::models::Post;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_query() {
+        let connection = establish_connection();
+        let results = posts.filter(published.eq(true))
+            .limit(5)
+            .load::<Post>(&connection)
+            .expect("Error loading posts");
+
+        println!("Displaying {} posts", results.len());
+        for post in results {
+            println!("{}", post.title);
+            println!("------------\n");
+            println!("{}", post.body);
+        }
     }
 }
---
+++ common/db/src/models/mod.rs
@@ -0,0 +1,4 @@
+
+pub mod models;
+
+pub use models::Post as Post;
